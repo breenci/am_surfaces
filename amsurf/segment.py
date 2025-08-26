a5a96a42a546fea5de51aabb7355c5532cf0e71a
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2025-08-26 15:39:17 +0100

    Add MAD segmentation method

--- amsurf/segment.py
+++ amsurf/segment.py
@@ -96,13 +96,109 @@ def glob_thresh(image_path: str, blur: bool = True, g_sigma: float = 2,
     return regions
 
 
+def threshold_mad(im: np.ndarray, k: float, invert: bool = False) -> float:
+    """Calculate threshold based on Median Absolute Deviation (MAD)
+    
+    see https://en.wikipedia.org/wiki/Median_absolute_deviation
+
+    Args:
+        im (np.ndarray): input image
+        k (float): scaling factor
+
+    Returns:
+        float: threshold value
+    """
+    # calculate the median and MAD
+    med = np.median(im)
+    mad = np.median(np.abs(im.astype(np.float32) - med))
+    # define the scaling factor for normal distribution
+    scaling = 1.4826
+    
+    if invert:
+        return med - mad * k * scaling
+    else:
+        return med + mad * k * scaling
+    
+
+def segment_mad(image_path: str, k: float = 5, invert: bool = True,
+                blur: bool = True, g_sigma: float = 2, 
+                clear_border: bool = True, 
+                crop: tuple[float, float, float, float] | None = None,
+                plot: bool =False) -> dict:
+    """
+    Implement MAD-based image segmentation
+
+    Args:
+        image_path (str): Path to the image file
+        k (float, optional): Scaling factor for MAD threshold. Defaults to 5.
+        invert (bool, optional): Select True, if regions of interest have pixel
+        values below the background. Otherwise, set to False. Defaults to True.
+        blur (bool, optional): If True, apply a Gaussian blur. Defaults to True.
+        g_sigma (float, optional): Standard deviation for Gaussian kernel. 
+        Defaults to 2.
+        clear_border (bool, optional): If True, remove any segments which touch
+        the border of the image or cropped region. Defaults to True.
+        crop (tuple[float, float, float, float] | None, optional): If not None, 
+        crops the input image to the region defined by tuple. The box is a 
+        4-tuple defining the left, upper, right, and lower pixel coordinate.
+        Defaults to None.
+        plot (bool, optional): _description_. Defaults to False.
+
+    Returns:
+       dict: region properties including label, centroid, area, and
+       equivalent_diameter_area
+    """
+    
+    # load image as grayscale
+    img = Image.open(image_path).convert("L")
+
+    # crop image if specified
+    if crop is not None:
+        img = img.crop(crop)
+    
+    img_arr = np.array(img)
+    
+    # apply gaussian blur if required
+    if blur:
+        img_arr = ski.filters.gaussian(img_arr, sigma=g_sigma)
+    
+    # calculate MAD threshold
+    threshold = threshold_mad(img_arr, k=k, invert=invert)
+    
+    # create binary image
+    if invert:
+        binary = img_arr < threshold
+    else:
+        binary = img_arr > threshold
+    
+    # fill holes
+    binary = ndi.binary_fill_holes(binary)
+    
+    # clear border if required
+    if clear_border:
+        binary = ski.segmentation.clear_border(binary)
+        
+    # label the image
+    label = ski.measure.label(binary)
+    
+    # region properties
+    regions = ski.measure.regionprops_table(label, 
+                                            properties=('label', 'centroid', 'area', 
+                                                        'equivalent_diameter_area'))
+    # plot the image if required
+    if plot:
+        plot_label_image(label, underlay_image=img_arr)
+    
+    return regions
+
+
 if __name__ == "__main__":
     # path_to_image_LPE0 = "data/project_sharepoint/02_Sample01__GL_concave/01_SEM_maps/20240724_LPE00_ArmSample_001_SESI.tif"
     # regions_LPE0 = OTSU(path_to_image_LPE0, blur=False, g_sigma=2, clear_border=True, crop=[0,0,2048,1400], plot=True)
     # pd.DataFrame(regions_LPE0).to_csv("data/processed/test_LPE0.csv")
     
     path_to_image_CAM1 = "data/project_sharepoint/04_Sample03_IA_flat/20250502_IA_CCAM_M5/Acquisition_08.tif"
-    regions_CAM1 = glob_thresh(path_to_image_CAM1, blur=False, g_sigma=2, clear_border=False, crop=None, plot=True, method="triangle")
+    regions_CAM1 = glob_thresh(path_to_image_CAM1, blur=True, g_sigma=2, clear_border=False, crop=None, plot=True, method="triangle")
     
     plt.show()
 
